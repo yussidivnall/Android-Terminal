0b834aa072b11d607b62a22b53e9fb7cfe03c201
uri <UC.dev.null@gmail.com>
2009-10-16 12:28:06 +0100

    A very simple terminal app for android to build on

---
+++ src/test/term2/TestTerm2.java
@@ -0,0 +1,83 @@
+package test.term2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class TestTerm2 extends Activity {
+	
+	
+	TextView termOut;
+	EditText termIn;
+	
+	Button enter_button;
+	EditText prompt_box;
+	
+	
+	termThread term;
+    public Handler handle = new Handler(){
+    	public void handleMessage(Message msg){
+    		termOut.append((String)msg.obj);
+    		scrollDown();
+    	}
+    };
+	
+	
+    /** Called when the activity is first created. */	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        startThread();
+        
+        termOut = (TextView)findViewById(R.id.termOutput);
+        prompt_box = (EditText)findViewById(R.id.Input);
+        
+        enter_button = (Button)findViewById(R.id.enter);
+        enter_button.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				enter_pressed();
+			}
+        });
+        
+        
+        
+        
+    }
+    
+    public void enter_pressed(){
+		String cmd = prompt_box.getText().toString();
+		//termOut.setTextColor(0x00ff00);
+		termOut.append(cmd+"\n");
+		term.exec(cmd+"\n");    	
+		scrollDown();
+
+		//sv.scrollTo(0, sv.getBaseline());
+    }
+    
+    
+    public void startThread(){
+        term = new termThread(this);
+        term.start();
+        term.exec("echo 'a shell of a shell!' \n");
+        
+    }
+    protected void onKeyDown(TextView t){
+    	
+    }
+    public void scrollDown(){
+		ScrollView sv = (ScrollView)findViewById(R.id.ScrollView01);
+		//sv.fullScroll(ScrollView.FOCUS_DOWN);
+		sv.scrollTo(0, termOut.getHeight());
+    }
+}
---
+++ src/test/term2/termThread.java
@@ -0,0 +1,83 @@
+package test.term2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import android.os.Message;
+
+public class termThread extends Thread implements Runnable {
+	private static final String SHELL="/system/bin/sh";
+	private boolean done = false;
+	
+	TestTerm2 parent;
+	
+	DataOutputStream stdIn;
+	DataInputStream stdOut;
+	DataInputStream stdErr;
+	
+	Process process;
+	
+	termThread(TestTerm2 p){
+		try {
+			parent = p;
+			
+			process = Runtime.getRuntime().exec(SHELL);
+			stdIn = new DataOutputStream(process.getOutputStream());
+			stdOut = new DataInputStream(process.getInputStream());
+			stdErr = new DataInputStream(process.getErrorStream());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void run(){
+		try {
+			Thread.sleep(100);
+			while (!done){
+				while( (stdOut.available()==0) && (stdErr.available()==0) ){ Thread.sleep(10);}
+				dump();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+    public void dump() {
+    	try{
+	    	String out ="";
+			if(stdOut.available()>0){
+				while(stdOut.available() > 0){
+					out+=""+(char)stdOut.read();
+				}						
+			}
+			
+			if(stdErr.available() >0 ){
+				while(stdErr.available() > 0){
+					out+=""+(char)stdErr.read();
+				}						
+			}
+			parent.handle.sendMessage(Message.obtain(parent.handle,0,out));
+    	}catch(IOException e){}
+    }
+    
+    public void exec(String command){
+    	try {
+			stdIn.write(command.getBytes());
+			stdIn.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    
+	
+}
